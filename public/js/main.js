e2620a0feedefde0987b7662258a7657ccc57e8a
NSTF <starlinu33@gmail.com>
2018-06-19 14:42:53 -0400

    both sides home and reminders is working well, particles.js library for the background finished too

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+
+  var myJSON = {
+    "particles": {
+      "number": {
+        "value": 200
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "size": {
+        "value": 2,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": 'random',
+        "straight": false
+      }
+    },
+    "interactivity": {
+      "detect_on": "particles-js",
+      "events": {
+        "onhover": {
+          "enable": true
+        }
+      },
+      "modes": {
+        "push": {
+          "particles_nb": 12
+        }
+      }
+    }
+  }
+
+  // initializing the library
+  particlesJS(myJSON);
+});
