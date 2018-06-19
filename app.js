67604fd658b33b733e8a9e12936b50c9606aafff
NSTF <starlinu33@gmail.com>
2018-06-19 11:24:44 -0400

    nav bar finished

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require(`express`);
+// server.js
+// load the things we need
+const express = require('express');
 const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 const port = 8888;
 const { Pool, Client } = require('pg');
 const parser = require('body-parser');
 app.use(parser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render(`home`, {
