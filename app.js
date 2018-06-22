25293a6428300b04c0a0c31b691cee7fed55172d
NSTF <starlinu33@gmail.com>
2018-06-22 03:18:18 -0400

    database and history link is finished correctly

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require(`express`);
 const app = express();
-const port = 3000;
-const { Pool, Client } = require('pg');
+const port = 8000;
+const {
+  Pool,
+  Client
+} = require('pg');
 const parser = require('body-parser');
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '#BeastMode27',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '#BeastMode27',
+  port: 5432,
 });
 
+let table = {};
 
 app.get('/', (req, res) => {
   res.render(`home`, {
@@ -23,17 +29,50 @@ app.get('/', (req, res) => {
 });
 
 app.get('/standings', (req, res) => {
-  res. render('standings', {
+  res.render('standings', {
 
   })
 });
 
 
-app.get('/reminders', (req, res) => {
-  res.render('reminder', {
+app.get('/stats', (req1, res1) => {
+  res1.render('stats', {
 
   })
+
 });
+
+app.get('/history', (req, res) => {
+  pool.query('SELECT * FROM history', (req2, res2) => {
+
+    tableObj = res2.rows;
+    console.log(tableObj);
+
+    res.render('history', {
+      data: tableObj
+    })
+  })
+})
+
+app.post('/post', (req, res) => {
+
+  var inserQuery = {
+    text: 'INSERT INTO history(history) VALUES($1)',
+    values: [req.body.search]
+  }
+
+  pool.query(inserQuery, (req, res) => {
+    console.log('BOOOOOOM, inserted........');
+  });
+
+  pool.end;
+})
+
+app.get('*', (req, res) => {
+  res.render('errorPath', {
+
+  })
+})
 
 app.listen(port || process.env.PORT, (req, res) => {
   console.log(`Listening to port ${port}.....`);
