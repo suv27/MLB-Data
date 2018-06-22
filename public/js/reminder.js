193b23ea0de01e32f6b2b07947aec5de69aed5bf
NSTF <starlinu33@gmail.com>
2018-06-22 00:56:31 -0400

    Able to look for a team and see all their player up to date stats, 75% done

--- public/js/reminder.js
+++ public/js/reminder.js
@@ -1,18 +1,11 @@
-var teamsObject = [];
-
 $(document).ready(function() {
 
-  let teamsID = [];
-  let teamsName = [];
-  let teamsMarket = [];
-  let teamsAbbr = [];
-  let dictionary = [];
-  let teamInfo = [];
-  let playersHolder = [];
-  let ids = [];
+  var teamsObject = [];
+  var teamsProfile = [];
+  let teamProfile = '';
   let sportradarApiKey = '656e58ujs4q8a5cbpcabebea';
-  let rankings = "http://api.sportradar.us/mlb/trial/v6.5/en/seasons/2018/REG/rankings.json?api_key=" + sportradarApiKey;
-
+  let keyPatrick = 'mevcburkuv7wpjvpnvc6gaw5';
+  let rankings = "http://api.sportradar.us/mlb/trial/v6.5/en/seasons/2018/REG/rankings.json?api_key=" + keyPatrick;
 
   // GETTING ALL THE ID'S FROM THE RANKING URL AND STORING IT INTO AN EMPTY ARRAY
   $.getJSON(rankings, function(info) {
@@ -26,84 +19,80 @@ $(document).ready(function() {
     let valOfSearchBox = event.target.value;
     let index = -1;
     let foundTeam = undefined;
-    //info.league.season.leagues[0].divisions[0].teams;
-    for (var i = 0; i < teamsObject.league.season.leagues.length && (foundTeam == undefined); i++)
-    {
+
+    for (var i = 0; i < teamsObject.league.season.leagues.length && (foundTeam == undefined); i++) {
       var thisLeague = teamsObject.league.season.leagues[i];
-      for (var j = 0; j < thisLeague.divisions.length && (foundTeam == undefined); j++)
-      {
+      for (var j = 0; j < thisLeague.divisions.length && (foundTeam == undefined); j++) {
         var thisDivision = thisLeague.divisions[j];
-        for (var k = 0; k < thisDivision.teams.length && (foundTeam == undefined); k++)
-        {
+        for (var k = 0; k < thisDivision.teams.length && (foundTeam == undefined); k++) {
           var thisTeam = thisDivision.teams[k];
+
           if (valOfSearchBox.toLowerCase() === thisTeam.name.toLowerCase() ||
-              valOfSearchBox.toLowerCase() === thisTeam.market.toLowerCase() ||
-              valOfSearchBox.toLowerCase() === thisTeam.abbr.toLowerCase()) {
+            valOfSearchBox.toLowerCase() === thisTeam.market.toLowerCase() ||
+            valOfSearchBox.toLowerCase() === thisTeam.abbr.toLowerCase()) {
             foundTeam = thisTeam;
-            console.log("Found team", foundTeam);
           }
         }
       }
     }
 
     if (foundTeam != undefined) {
-      let teamProfile = `http://api.sportradar.us/mlb/trial/v6.5/en/teams/${thisTeam.id}/profile.json?api_key=` + sportradarApiKey;
 
+      let teamProfile = `http://api.sportradar.us/mlb/trial/v6.5/en/teams/${thisTeam.id}/profile.json?api_key=` + keyPatrick;
       $.getJSON(teamProfile, function(info) {
+        teamsProfile = info;
 
-        let team = info;
-        let teamName = info.name;
-        let teamMarket = info.market;
-        let leagueName = info.league.name;
-        let divisionName = info.division.name;
-        let listOfPlayers = info.players;
-        let player = listOfPlayers;
+        let player = teamsProfile.players;
 
-          $('.innerTeamInfo').html(
-            `<tr>
-                <td> ${teamName} </td>
-                <td> ${teamMarket}</td>
-                <td> ${leagueName} </td>
-                <td> ${divisionName} </td>
-            </tr>`
-          )
+        $('.innerTeamInfo').html(
+          `<tr><td> ${info.name} </td><td> ${info.market}</td><td> ${info.league.name} </td><td> ${info.division.name} </td></tr>`
+        )
 
-        $('.innerPlayerInfo').html('');
         for (var i = 0; i < player.length; i++) {
+
+          let playerObjectHitting;
+          let playerObjectFielding;
+
+          // <td> ${playerObjectHitting.statistics.hitting.overall.games.play}</td>
+          // <td> ${playerObjectHitting.statistics.hitting.overall.ab}</td>
+          // <td> ${playerObjectHitting.statistics.hitting.overall.runs.total}</td>
+          // <td> ${playerObjectHitting.splits.hitting.overall[0].total.h}</td>
+
           $('.innerPlayerInfo').append(
             `<tr>
-                <td> ${player[i].full_name} </td>
-                <td> ${team.abbr}</td>
-                <td> ${player[i].position} </td>
-                <td> ${player[i].jersey_number} </td>
-                <td> ${player[i].throw_hand} </td>
-                <td> ${player[i].bat_hand} </td>
-                <td> ${player[i].pro_debut} </td>
-            </tr>`
+                <td> ${teamsProfile.players[i].full_name} </td>
+                <td> ${info.abbr}</td>
+                <td> ${teamsProfile.players[i].position} </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> ${teamsProfile.players[i].jersey_number} </td>
+                <td> ${teamsProfile.players[i].throw_hand} </td>
+                <td> ${teamsProfile.players[i].bat_hand} </td>
+                <td> ${teamsProfile.players[i].pro_debut} </td>
+              </tr>`
           )
+
+          // let playerProfile = `http://api.sportradar.us/mlb/trial/v6.5/en/players/${player[i].id}/profile.json?api_key=` + keyPatrick;
+          // $.getJSON(playerProfile, function(data) {
+          //
+          //   $('.innerPlayerInfo').html('');
+          //
+          //   playerObjectHitting = data.player.seasons[0].totals.statistics;
+          //   playerObjectFielding = data.player.seasons[0].totals.statistics.fielding;
+          // })
         }
       })
 
-      //let playerProfile = `http://api.sportradar.us/mlb/trial/v6.5/en/players/${dictionary[index].playersID}/profile.json?api_key=` + sportradarApiKey;
-      //let playerInfo = playerProfile;
-      // console.log(playerInfo);
-      // console.log(dictionary[index].playersID);
-      // $.getJSON(playerProfile, function(data) {
-      //
-      // GETTING ALL THE PLAYERS ID's
-      // playersHolder.push(data.player);
-      // console.log(playersHolder.id);
-
-      // for (var i = 0; i < dictionary.length; i++) {
-      // console.log('hola');
-      // console.log(playersHolder);
-      // playersID.push(ids[i].id);
-      // }
-
-      // })
-
-
     }
-
   })
 });
