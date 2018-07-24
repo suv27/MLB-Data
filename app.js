99846a0d1dc06a092e66e88c7944762773aee258
NSTF <starlinu33@gmail.com>
2018-07-24 00:48:25 -0400

    fixed some errors, converted from px to percentage all images in the home tab

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 8000;
 const {
   Pool,
-  Client
+  Client,
 } = require('pg');
 const parser = require('body-parser');
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -19,32 +19,24 @@ const pool = new Pool({
   port: 5432,
 });
 
-// const pool = new Pool ({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// })
-
-
 let table = {};
 
 app.get('/', (req, res) => {
   res.render(`home`, {
 
-  })
+  });
 });
 
 app.get('/standings', (req, res) => {
   res.render('standings', {
 
-  })
+  });
 });
-
 
 app.get('/stats', (req1, res1) => {
   res1.render('stats', {
 
-  })
-
+  });
 });
 
 app.get('/history', (req, res) => {
@@ -53,45 +45,44 @@ app.get('/history', (req, res) => {
     tableObj = res2.rows;
 
     res.render('history', {
-      data: tableObj
-    })
-  })
-})
+      data: tableObj,
+    });
+  });
+});
 
 app.post('/post', (req, res) => {
 
   var inserQuery = {
     text: 'INSERT INTO history(history) VALUES($1)',
-    values: [req.body.search]
-  }
+    values: [req.body.search],
+  };
 
   pool.query(inserQuery, (req, res) => {
     console.log('BOOOOOOM, inserted........');
   });
-
-})
+});
 
 // DELETING THE REQUEST THAT I GOT FORM THE AJAX FROM THE DATABASE
 app.delete('/delete/:id', (req, res) => {
 
   var deleteQuery = {
     text: 'DELETE FROM history WHERE id = $1',
-    values: [req.params.id]
-  }
+    values: [req.params.id],
+  };
 
   pool.query(deleteQuery, (req, res) => {
 
   });
 
-  res.redirect('/history')
+  res.redirect('/history');
 
 });
 
 app.get('*', (req, res) => {
   res.render('errorPath', {
 
-  })
-})
+  });
+});
 
 app.listen(process.env.PORT || port, (req, res) => {
   console.log(`Listening to port ${port}.....`);
